930e86b01bc35b59e54e533b221f95e934495d00
Bishwadeep Sikder <rahulsikder223@gmail.com>
2020-03-26 11:49:18 +0530

    Socket Server and Client

---
+++ Server/SocketServer/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using SocketServer.Socket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SocketServer.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpPost]
+        [ActionName("Trigger")]
+        public bool TestMethod([FromBody] string message)
+        {
+            SocketUtility socketObject = SocketUtility.GetInstance();
+            socketObject.BroadcastMessage(message);
+            return true;
+        }
+    }
+}
---
+++ Server/SocketServer/Socket/SocketUtility.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SuperSocket.SocketBase;
+using SuperSocket.WebSocket;
+
+
+namespace SocketServer.Socket
+{
+    public class SocketUtility
+    {
+        private static WebSocketServer _wsServer;
+        private static SocketUtility _socketUtilityObject;
+
+        public static SocketUtility GetInstance()
+        {
+            return _socketUtilityObject;
+        }
+
+        public static void CreateInstance()
+        {
+            if (_socketUtilityObject == null)
+            {
+                _socketUtilityObject = new SocketUtility();
+            }
+        }
+
+        SocketUtility()
+        {
+            InitializeSocket();
+        }
+
+        public void InitializeSocket()
+        {
+            _wsServer = new WebSocketServer();
+            int port = 8800;
+            _wsServer.Setup(port);
+            _wsServer.NewSessionConnected += WsServer_NewSessionConnected;
+            _wsServer.NewDataReceived += WsServer_NewDataReceived;
+            _wsServer.NewMessageReceived += WsServer_NewMessageReceived;
+            _wsServer.SessionClosed += WsServer_SessionClosed;
+            _wsServer.Start();
+        }
+
+        public void BroadcastMessage(string value)
+        {
+            if (value != null)
+            {
+                foreach (var s in _wsServer.GetAllSessions())
+                {
+                    s.Send(value);
+                }
+            }
+        }
+
+        private static void WsServer_SessionClosed(WebSocketSession session, CloseReason value)
+        {
+            Console.WriteLine("SessionClosed");
+        }
+
+        private static void WsServer_NewMessageReceived(WebSocketSession session, string value)
+        {
+            Console.WriteLine("NewMessageReceived: " + value);
+            if (value != null)
+            {
+                session.Send("Hello from the other side...");
+            }
+        }
+
+        private static void WsServer_NewDataReceived(WebSocketSession session, byte[] value)
+        {
+            Console.WriteLine("NewDataReceived");
+        }
+
+        private static void WsServer_NewSessionConnected(WebSocketSession session)
+        {
+            Console.WriteLine("NewSessionConnected");
+        }
+    }
+}
